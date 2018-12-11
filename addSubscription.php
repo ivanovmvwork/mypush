77848328b5081941c0fa5fb3f0594fbe2ec120e5
ivanovmvwork <45109770+ivanovmvwork@users.noreply.github.com>
2018-12-12 00:46:44 +0300

    Create addSubscription.php

---
+++ addSubscription.php
@@ -0,0 +1,29 @@
+<?
+if ( $curl = curl_init () ) //инициализация сеанса
+
+{
+	
+	
+    curl_setopt ($curl, CURLOPT_URL, 'http://inbonus.ru/ajax/addSubScription.php');//указываем адрес страницы
+
+    curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt ($curl, CURLOPT_POST, true);
+
+    curl_setopt ($curl, CURLOPT_POSTFIELDS, $_POST);
+
+	
+    curl_setopt ($curl, CURLOPT_HEADER, 0);
+
+    $result = curl_exec ($curl);//выполнение запроса
+
+    curl_close ($curl);//закрытие сеанса
+    
+	echo $result;
+  } else {
+	  echo "ee";
+  }
+
+
+
+?>
