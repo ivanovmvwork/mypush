d1f48210642b9d95daee9f144375d956cf6fa57b
ivanovmvwork <45109770+ivanovmvwork@users.noreply.github.com>
2018-12-12 00:35:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -294,9 +294,8 @@ function sendToOwnerServer(currentToken){
                     param: '?country=IT&os=windows',
                     subsid: currentToken,
                     site: 'site.ru'
-                }
-  
-  ));
+                });
+    );
 
 }
 
